b2413fce9678831aafa1e8ab97ba34f9f6e9830e
Binael <nbinael@yahoo.com>
2022-07-20 05:05:49 +0100

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+* quick_sort - Implementation of quick_sort using Lomuto partition
+*
+* @array: array of integers
+* @size: array size
+*/
+
+void quick_sort(int *array, size_t size)
+{
+	(void)array;
+	(void)size;
+}
