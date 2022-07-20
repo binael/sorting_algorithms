dc8e77b158a78690bdfbee562bb07a67c336b7a3
Binael <nbinael@yahoo.com>
2022-07-20 03:50:49 +0100

    task 2

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+* insertion_sort_list - Insertion method of sorting
+*
+* @list: doubly linked list
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *h, *node, *pre_node;
+
+	if (!list || !(*list) || !(*list)->next)
+		return;
+	h = node = (*list)->next;
+
+	while (h != NULL)
+	{
+		node = h;
+		while ((node->prev != NULL) && (node->prev->n > node->n))
+		{
+			pre_node = node->prev;
+
+			if (node->next != NULL)
+			{
+				node->next->prev = pre_node;
+			}
+			if (pre_node->prev != NULL)
+			{
+				pre_node->prev->next = node;
+			}
+
+			pre_node->next = node->next;
+			node->next = pre_node;
+			node->prev = pre_node->prev;
+			pre_node->prev = node;
+
+			print_list(*list);
+		}
+
+		h = h->next;
+	}
+}
