4410258574e68816115d311464acae4e55d28e7d
Binael <nbinael@yahoo.com>
2022-07-20 05:19:36 +0100

    empty templates

---
+++ 100-shell_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+* shell_sort - Implement shell sort sorting
+*
+* @array: array of integers
+* @size: size of array
+*/
+
+void shell_sort(int *array, size_t size)
+{
+	(void)array;
+	(void)size;
+}
---
+++ 105-radix_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+* radix_sort - Implement the radix sorting
+*
+* @array: array of integers
+* @size: size of array
+*/
+
+void radix_sort(int *array, size_t size)
+{
+	(void)array;
+	(void)size;
+}
